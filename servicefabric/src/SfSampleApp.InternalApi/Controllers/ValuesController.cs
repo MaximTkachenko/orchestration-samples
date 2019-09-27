48d6bc24cc43c4b7d06fbb3f49c0679818aab5ba
mtkachenko <oblomov86@gmail.com>
2019-09-27 14:58:47 +0200

    added sf publish profiles for dev and prod

--- servicefabric/src/SfSampleApp.InternalApi/Controllers/ValuesController.cs
+++ servicefabric/src/SfSampleApp.InternalApi/Controllers/ValuesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SfSampleApp.InternalApi.Controllers
@@ -7,6 +8,13 @@ namespace SfSampleApp.InternalApi.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly IHostingEnvironment _env;
+
+        public ValuesController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
         [HttpGet]
         [Route("api/values")]
         public ActionResult<IEnumerable<string>> Get()
@@ -21,7 +29,8 @@ namespace SfSampleApp.InternalApi.Controllers
             return new Dictionary<string, string>
             {
                 {"ASPNETCORE_ENVIRONMENT", Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")},
-                {"APPINSIGHTS_INSTRUMENTATIONKEY", Environment.GetEnvironmentVariable("APPINSIGHTS_INSTRUMENTATIONKEY")}
+                {"APPINSIGHTS_INSTRUMENTATIONKEY", Environment.GetEnvironmentVariable("APPINSIGHTS_INSTRUMENTATIONKEY")},
+                {"EnvironmentName", _env.EnvironmentName}
             };
         }
     }
